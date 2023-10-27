af8f73d8281f0193b30ed30b2ff05eca08c57d31
Lucas Nascimento <lucasnascimento.lp@gmail.com>
2023-10-27 10:37:20 -0300

    refactor: :recycle: muda nome de varíavel
    Nome de variável alterado para cumprir regra do lint

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ function colorSelected({ target }) {
 }
 
 function colorPixel(event) {
-  const selectedBackgroundColor = getComputedStyle(document.querySelector('.selected')).backgroundColor;
+  const selectedBackground = getComputedStyle(document.querySelector('.selected')).backgroundColor;
   const { target } = event;
   const currentPixelColor = getComputedStyle(target).backgroundColor;
 
-  if (currentPixelColor === selectedBackgroundColor) {
+  if (currentPixelColor === selectedBackground) {
     target.style.backgroundColor = 'white';
   } else {
-    target.style.backgroundColor = selectedBackgroundColor;
+    target.style.backgroundColor = selectedBackground;
   }
 }
 
