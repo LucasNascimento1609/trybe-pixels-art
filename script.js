5059c87e2e4662225948abd5f8b5916db1349f4c
Lucas Nascimento <lucasnascimento.lp@gmail.com>
2023-10-27 09:54:08 -0300

    feat: :sparkles: adiciona a função generatBoard
    A função permite ao usuário gerar uma quadro de 5x5 à 50x50

--- script.js
+++ script.js
@@ -23,6 +23,41 @@ function clearBoard() {
   }
 }
 
+function validateBoard(boardSize) {
+  if (boardSize < 5) return 5;
+  if (boardSize > 50) return 50;
+  if (Number.isNaN(boardSize)) return false;
+  return boardSize;
+}
+
+function resetPixelBoard(boardSize) {
+  const pixelBoard = document.getElementById('pixel-board');
+  pixelBoard.innerHTML = '';
+  pixelBoard.style.gridTemplateColumns = `repeat(${boardSize}, 40px)`;
+}
+
+function generateBoard() {
+  const boardSize = parseInt(document.getElementById('board-size').value, 10);
+
+  const validBoard = validateBoard(boardSize);
+  if (!validBoard) return alert('Board inválido!');
+  resetPixelBoard(validBoard);
+
+  for (let i = 0; i < validBoard; i += 1) {
+    for (let j = 0; j < validBoard; j += 1) {
+      const pixel = document.createElement('div');
+      pixel.classList.add('pixel');
+      pixel.style.backgroundColor = 'white';
+      pixel.addEventListener('click', colorPixel);
+      document.getElementById('pixel-board').appendChild(pixel);
+    }
+  }
+}
+
+function handleEnterKey(event) {
+  if (event.key === 'Enter') generateBoard();
+}
+
 const colors = document.getElementsByClassName('color');
 for (let index = 0; index < colors.length; index += 1) {
   colors[index].addEventListener('click', colorSelected);
@@ -34,3 +69,4 @@ for (let index = 0; index < pixels.length; index += 1) {
 }
 
 document.getElementById('clear-board').addEventListener('click', clearBoard);
+document.getElementById('generate-board').addEventListener('click', generateBoard);document.getElementById('board-size').addEventListener('keydown', handleEnterKey);
